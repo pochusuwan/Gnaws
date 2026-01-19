5eb575192966acff55e7c767161ab3494a49aaae
Siwakorn Chusuwan <sc2pu@virginia.edu>
2026-01-19 02:17:34 -0500

    Add server stats script

---
+++ game_server/scripts/server_stats.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eu
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# Read configurable values
+# SERVER_PORT_PROTOCOL "tcp" or "udp"
+# SERVER_PORT_NUMBER
+. "$SCRIPT_DIR/gnaws-script.conf"
+
+PROTOCOL="t"
+if [ "$SERVER_PORT_PROTOCOL" = "udp" ]; then
+    PROTOCOL="u"
+fi
+
+IS_ONLINE="false"
+if ss -ln$PROTOCOL | grep -q "[.:]$SERVER_PORT_NUMBER\b"; then
+    # output "online\n" exactly to signal online
+    IS_ONLINE="true"
+fi
+
+ACTIVE_PLAYERS=$(iptables -L INPUT -n -v -m recent --name GNAWS --rcheck --seconds 300 | grep GNAWS | wc -l)
+CURRENT_STORAGE_GIB=$(df --output=used -h -B1 . | tail -n1)
+MAX_STORAGE_GIB=$(df --output=size -h -B1 . | tail -n1)
+
+echo "is_online=$IS_ONLINE"
+echo "players_count=$ACTIVE_PLAYERS"
+echo "current_storage=$CURRENT_STORAGE_GIB"
+echo "max_storage=$MAX_STORAGE_GIB"
