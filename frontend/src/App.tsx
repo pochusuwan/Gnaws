456e2fc5cd2a0e4c5337d88a6ddb8753f6af2d7e
Siwakorn Chusuwan <sc2pu@virginia.edu>
2026-01-31 17:45:39 -0500

    Add logout and page selector

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,46 @@
 import { useState } from "react";
 import "./App.css";
 import type { User } from "./types";
-import { UserContext, useUser } from "./hooks/useUser";
-import LoginForm from "./components/Login/Login.tsx";
+import { UserContext } from "./hooks/useUser";
+import LoginForm, { LoggedIn } from "./components/Login/Login.tsx";
+import ServerPage from "./pages/ServerPage/ServerPage.tsx";
+import UserPage from "./pages/UserPage/UserPage.tsx";
+
+enum Page {
+    Servers = "Servers",
+    Users = "Users",
+}
 
 function App() {
     const [user, setUser] = useState<User | null>(null);
+    const [page, setPage] = useState<Page>(Page.Servers);
     if (!user) {
         return <LoginForm setUser={setUser} />;
     }
     return (
         <UserContext.Provider value={user}>
-            <Content />
+            <div className="app">
+                <LoggedIn clearUser={() => setUser(null)} />
+                <PageSelector current={page} onSelect={setPage} />
+                {page === Page.Servers && <ServerPage />}
+                {page === Page.Users && <UserPage />}
+            </div>
         </UserContext.Provider>
     );
 }
 
-function Content() {
-    const user = useUser();
-
+type PageSelectorProps = {
+    current: Page;
+    onSelect: (page: Page) => void;
+};
+function PageSelector({ current, onSelect }: PageSelectorProps) {
     return (
-        <div>
-            {user?.username} {user?.role}
+        <div className="pageSelector">
+            {Object.values(Page).map((page) => (
+                <button key={page} onClick={() => onSelect(page)} disabled={current === page}>
+                    {page}
+                </button>
+            ))}
         </div>
     );
 }
