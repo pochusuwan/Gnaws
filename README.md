f21c375bfc21666146ab35b5521ce60c4d1377d4
ryuyano <ryuyano15@gmail.com>
2026-02-11 20:28:18 -0500

    Injecting domain name and ARN for Amazon certificate

--- README.md
+++ README.md
@@ -12,3 +12,11 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `npx cdk deploy`  deploy this stack to your default AWS account/region
 * `npx cdk diff`    compare deployed stack with current state
 * `npx cdk synth`   emits the synthesized CloudFormation template
+
+## Custom Domain
+Example deploy command:
+```bash
+CLOUDFRONT_DOMAIN_NAME=app.example.com \
+CLOUDFRONT_CERT_ARN=arn:aws:acm:us-east-1:123456789012:certificate/xxxx \
+npx cdk deploy
+```
--- bin/gnaws.ts
+++ bin/gnaws.ts
@@ -2,26 +2,16 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { GnawsStack } from '../lib/gnaws-stack';
 
+const cloudFrontDomainName = process.env.CLOUDFRONT_DOMAIN_NAME;
+const cloudFrontCertArn = process.env.CLOUDFRONT_CERT_ARN;
+
 const app = new cdk.App();
 new GnawsStack(app, 'GnawsStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Optional: Configure a custom domain for the CloudFront front end
-   *
-   * - If you set `cloudFrontDomainName`, you must also set `cloudFrontCertArn`.
-   * - NOTE: CloudFront only supports ACM certs in us-east-1
-  // cloudFrontDomainName: "example.com",
-  // cloudFrontCertArn:"arn:aws:acm:us-east-1:<AMAZON ARN FOR YOUR CERTIFICATE>",
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  ...(cloudFrontDomainName && cloudFrontCertArn
+    ? { cloudFrontDomainName, cloudFrontCertArn,}
+    : {}),
 });
