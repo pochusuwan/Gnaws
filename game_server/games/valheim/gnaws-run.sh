04d9c6101c081dbd5e20ff52901d0b9fd2a32255
Siwakorn Chusuwan <sc2pu@virginia.edu>
2026-02-23 20:06:59 -0500

    Add exec to run scripts

--- game_server/games/valheim/gnaws-run.sh
+++ game_server/games/valheim/gnaws-run.sh
@@ -9,6 +9,6 @@ echo "Starting server PRESS CTRL-C to exit"
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-./valheim_server.x86_64 -name "Gnaws Server" -port 2456 -world "Dedicated" -password "secret" -crossplay -savedir "$SCRIPT_DIR/save"
+exec ./valheim_server.x86_64 -name "Gnaws Server" -port 2456 -world "Dedicated" -password "secret" -crossplay -savedir "$SCRIPT_DIR/save"
 
 export LD_LIBRARY_PATH=$templdpath
